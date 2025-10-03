cc68f44a9481c3ccdc8b1dc18d11a2986589c83e
Pankaj Kumar Ray <techyray35@gmail.com>
2025-10-04 05:13:07 +0530

    Folder Structure Modified for Full Stack App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import CouncilPage from './components/CouncilPage/CouncilPage.jsx'
+
+function App() {
+  return (
+    <>
+    <CouncilPage heading="CODOKO COUNCIL LOGIN"/>
+    <h1 style={{fontFamily:"roboto"}}>Pankaj </h1>
+    </>
+  )
+}
+export default App
---
+++ frontend/src/components/CouncilPage/CouncilPage.jsx
@@ -0,0 +1,15 @@
+
+function CouncilPage({ heading }) {
+
+    return (
+
+        <div className="Council">
+
+
+            <h2 className="font-inter">{heading}</h2>
+        </div>
+
+    )
+}
+
+export default CouncilPage
