921dbf28f8d9d136739015b00fbcf49c92198794
Aarzoo <techyray35@gmail.com>
2026-01-29 19:50:20 +0530

    Iniating backend of CODOKO

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -2,23 +2,14 @@ import mongoose from "mongoose";
 import { app } from "../app.js";
 import DB_NAME from "../constants.js";
 
-
-
-
-const dbConnect = async()=>{
+const dbConnect = async () => {
     try {
-
         const connInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
-
-       
-
-        console.log(`Connected to codokoclubdev Database ✅  `,connInstance.connection.host);
-        
+        console.log(`Connected to codokoclubdev Database ✅  `, connInstance.connection.host);
     } catch (err) {
-         console.log("ERROR in DB Folder: ",err);
+        console.log("ERROR in DB Folder: ", err);
         process.exit(1);
     }
-
 }
 
 export default dbConnect
