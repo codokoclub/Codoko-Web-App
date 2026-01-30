30e97e02971b8b0b8dd30f0df5dc09521718fea5
Aarzoo <techyray35@gmail.com>
2026-01-30 10:16:05 +0530

    Model: Done and Dusted for Dashboard

--- backend/src/model/socialHanldes.model.js
+++ backend/src/model/socialHanldes.model.js
@@ -3,6 +3,13 @@ import mongoose,{Schema,model} from "mongoose";
 
 const socialHandelSchema = new Schema({
 
-    
+
+    socialMediaLink:{
+        type:String
+    }
 
 },{timestamps:true})
+
+
+
+export const SocialHandel = mongoose.model("SocialHandle",socialHandelSchema)
